5d89acca265321f2337e4cbc449779d17ea63233
Px7-941 <tedbits@mailbox.org>
2017-05-17 07:32:46 +0200

    Add working project

---
+++ NoLockScreen/App.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows;
+using Hardcodet.Wpf.TaskbarNotification;
+
+namespace NoLockScreen
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        static Mutex _instanceMutex;
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+
+        [Flags]
+        public enum EXECUTION_STATE : uint
+        {
+            ES_AWAYMODE_REQUIRED = 0x00000040,
+            ES_CONTINUOUS = 0x80000000,
+            ES_DISPLAY_REQUIRED = 0x00000002,
+            ES_SYSTEM_REQUIRED = 0x00000001
+            // Legacy flag, should not be used.
+            // ES_USER_PRESENT = 0x00000004
+        }
+
+        private TaskbarIcon _notifyIcon;
+
+        public App()
+        {
+            InitializeComponent();
+            Current.Exit += (sender, e) => SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS);
+        }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var attribute = (GuidAttribute)assembly.GetCustomAttributes(typeof(GuidAttribute), true)[0];
+            var guid = attribute.Value;
+
+            bool createdNew;
+            _instanceMutex = new Mutex(true, guid, out createdNew);
+            if (!createdNew)
+            {
+                _instanceMutex = null;
+                Current.Shutdown();
+                return;
+            }
+            base.OnStartup(e);
+
+            //create the notifyicon (it's a resource declared in NotifyIconResources.xaml
+            _notifyIcon = (TaskbarIcon)FindResource("NotifyIcon");
+            SetThreadExecutionState(EXECUTION_STATE.ES_DISPLAY_REQUIRED | EXECUTION_STATE.ES_CONTINUOUS);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            if (_notifyIcon != null)
+                _notifyIcon.Dispose(); //the icon would clean up automatically, but this is cleaner
+            if (_instanceMutex != null)
+                _instanceMutex.ReleaseMutex();
+            base.OnExit(e);
+        }
+    }
+}
